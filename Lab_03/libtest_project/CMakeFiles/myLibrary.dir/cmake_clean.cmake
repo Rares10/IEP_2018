dc9d2cccf3b088f4f46606bfc99c5c4138c2cb5b
Rares10 <raresneamu@yahoo.ro>
2018-10-17 11:37:59 +0300

    Made the app_project with CMake

---
+++ Lab_03/libtest_project/CMakeFiles/myLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myLibrary.dir/myLibrary.c.o"
+  "libmyLibrary.pdb"
+  "libmyLibrary.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Lab_03/libtest_project/myLibrary.c
+++ Lab_03/libtest_project/myLibrary.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
 {
-	printf("Hello\n");
+	printf("I am a library\n");
 	return 0;
 }
